0d49773e97543d80c974f2c80ff4d7bcbcce26fb
zyX2MH <zyX2MH@cyber-dojo.org>
2024-04-03 23:58:12 +0000

    7 ran tests, no prediction, got green

--- files/ChristmasSong.java
+++ files/ChristmasSong.java
@@ -2,7 +2,11 @@
 public class ChristmasSong {
 
     public static String createVerseHeader(int number) {
+        if (number ==1)
         return "On the first day of Christmas\n" +
+                "My true love gave to me:";
+        else 
+            return "On the second day of Christmas\n" +
                 "My true love gave to me:";
     }
 }
