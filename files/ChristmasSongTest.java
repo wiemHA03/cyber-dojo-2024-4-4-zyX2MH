1a60cfda61b2f1f728ade166f9a6ec34a2786c46
zyX2MH <zyX2MH@cyber-dojo.org>
2024-04-03 23:59:26 +0000

    8 ran tests, no prediction, got green

--- files/ChristmasSongTest.java
+++ files/ChristmasSongTest.java
@@ -9,10 +9,18 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ChristmasSongTest {
     
      @Test
-    void test_two_lines_of_first_verse() {
+    void test_first_two_lines_of_first_verse() {
         String expected = "On the first day of Christmas\n" +
                 "My true love gave to me:";
         String actual = ChristmasSong.createVerseHeader(1);
+        assertEquals(expected, actual);
+    }
+    
+      @Test
+    void test_first_two_lines_of_second_verse() {
+        String expected = "On the second day of Christmas\n" +
+                "My true love gave to me:";
+        String actual = ChristmasSong.createVerseHeader(2);
         assertEquals(expected, actual);
     }
 }
