44c80d505493b52fce12699f14c38ed2fcabf0cd
zyX2MH <zyX2MH@cyber-dojo.org>
2024-04-04 20:25:01 +0000

    52 ran tests, no prediction, got red

--- files/ChristmasSong.java
+++ files/ChristmasSong.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class ChristmasSong {
 
     public static String createVerseHeader(int number) {
-       List<String> dayList = List.of("first", "second", "third", "fourth", "fifth");
+       List<String> dayList = List.of("first", "second", "third", "fourth", "fifth", "sixth","seventh", "eight", "ninth", "tenth", "eleventh", "twelfth");
         return "On the " + dayList.get(number - 1) + " day of Christmas\n" +
                 "My true love gave to me:";
     
