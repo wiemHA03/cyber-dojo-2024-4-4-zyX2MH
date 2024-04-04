88fef0b266a07e0330597d87cbb09f128763efd7
zyX2MH <zyX2MH@cyber-dojo.org>
2024-04-04 20:20:44 +0000

    47 ran tests, no prediction, got amber

--- files/ChristmasSongTest.java
+++ files/ChristmasSongTest.java
@@ -54,4 +54,129 @@ public class ChristmasSongTest {
         String actual = ChristmasSong.createWholeVerse(3);
         assertEquals(expected, actual);
     }
+    
+         @Test
+    void test_whole_poem() {
+        String expected = "On the first day of Christmas\n" +
+                "My true love gave to me:\n" +
+                "A partridge in a pear tree.\n" +
+                "\n" +
+                "On the second day of Christmas\n" +
+                "My true love gave to me:\n" +
+                "Two turtle doves and\n" +
+                "A partridge in a pear tree.\n" +
+                "\n" +
+                "On the third day of Christmas\n" +
+                "My true love gave to me:\n" +
+                "Three french hens\n" +
+                "Two turtle doves and\n" +
+                "A partridge in a pear tree.\n" +
+                "\n" +
+                "On the fourth day of Christmas\n" +
+                "My true love gave to me:\n" +
+                "Four calling birds\n" +
+                "Three french hens\n" +
+                "Two turtle doves and\n" +
+                "A partridge in a pear tree.\n" +
+                "\n" +
+                "On the fifth day of Christmas\n" +
+                "My true love gave to me:\n" +
+                "Five golden rings\n" +
+                "Four calling birds\n" +
+                "Three french hens\n" +
+                "Two turtle doves and\n" +
+                "A partridge in a pear tree.\n" +
+                "\n" +
+                "On the sixth day of Christmas,\n" +
+                "My true love gave to me:\n" +
+                "Six geese a-laying\n" +
+                "Five golden rings\n" +
+                "Four calling birds\n" +
+                "Three french hens\n" +
+                "Two turtle doves and\n" +
+                "A partridge in a pear tree.\n" +
+                "\n" +
+                "On the seventh day of Christmas,\n" +
+                "My true love gave to me:\n" +
+                "Seven swans a-swimming\n" +
+                "Six geese a-laying\n" +
+                "Five golden rings\n" +
+                "Four calling birds\n" +
+                "Three french hens\n" +
+                "Two turtle doves and\n" +
+                "A partridge in a pear tree.\n" +
+                "\n" +
+                "On the eight day of Christmas,\n" +
+                "My true love gave to me:\n" +
+                "Eight maids a-milking\n" +
+                "Seven swans a-swimming\n" +
+                "Six geese a-laying\n" +
+                "Five golden rings\n" +
+                "Four calling birds\n" +
+                "Three french hens\n" +
+                "Two turtle doves and\n" +
+                "A partridge in a pear tree.\n" +
+                "\n" +
+                "On the ninth day of Christmas,\n" +
+                "My true love gave to me:\n" +
+                "Nine ladies dancing\n" +
+                "Eight maids a-milking\n" +
+                "Seven swans a-swimming\n" +
+                "Six geese a-laying\n" +
+                "Five golden rings\n" +
+                "Four calling birds\n" +
+                "Three french hens\n" +
+                "Two turtle doves and\n" +
+                "A partridge in a pear tree.\n" +
+                "\n" +
+                "On the tenth day of Christmas,\n" +
+                "My true love gave to me:\n" +
+                "Ten lords a-leaping\n" +
+                "Nine ladies dancing\n" +
+                "Eight maids a-milking\n" +
+                "Seven swans a-swimming\n" +
+                "Six geese a-laying\n" +
+                "Five golden rings\n" +
+                "Four calling birds\n" +
+                "Three french hens\n" +
+                "Two turtle doves and\n" +
+                "A partridge in a pear tree.\n" +
+                "\n" +
+                "On the eleventh day of Christmas,\n" +
+                "My true love gave to me:\n" +
+                "Eleven pipers piping\n" +
+                "Ten lords a-leaping\n" +
+                "Nine ladies dancing\n" +
+                "Eight maids a-milking\n" +
+                "Seven swans a-swimming\n" +
+                "Six geese a-laying\n" +
+                "Five golden rings\n" +
+                "Four calling birds\n" +
+                "Three french hens\n" +
+                "Two turtle doves and\n" +
+                "A partridge in a pear tree.\n" +
+                "\n" +
+                "On the twelfth day of Christmas,\n" +
+                "My true love gave to me:\n" +
+                "Twelve drummers drumming\n" +
+                "Eleven pipers piping\n" +
+                "Ten lords a-leaping\n" +
+                "Nine ladies dancing\n" +
+                "Eight maids a-milking\n" +
+                "Seven swans a-swimming\n" +
+                "Six geese a-laying\n" +
+                "Five golden rings\n" +
+                "Four calling birds\n" +
+                "Three french hens\n" +
+                "Two turtle doves and\n" +
+                "A partridge in a pear tree.";
+       
+      
+        StringBuilder poem = new StringBuilder();
+        for (int i = 1; i <= 12; i++) {
+            poem.append(new StringBuilder(createVerseHeader(number)));
+            poem.append("/n");
+        }
+        assertEquals(expected, poem.toString());
+    }
 }
